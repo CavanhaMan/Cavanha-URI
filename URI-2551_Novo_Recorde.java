8cbb63ffe1da7144955b96b0f915c84545693d84
CavanhaMan <cavanhaman@hotmail.com>
2020-10-02 02:46:30 -0300

    URI 2551 enviado ok

---
+++ URI-2551_Novo_Recorde.java
@@ -0,0 +1,32 @@
+package projeto01;
+/*-------------------*
+| Rodrigo CavanhaMan |
+| URI 2551           |
+| Novo Recorde       |
+*--------------------*/
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.ENGLISH);
+		Locale.setDefault(new Locale("en", "US"));
+		
+		while (sc.hasNext()) {
+			int N = sc.nextInt(); //num de treinos feitos
+			double R = 0; //para calcular o record
+			
+			for (int i=1 ; i<=N ; i++) {
+				Double T = sc.nextDouble(); //duracao do treino
+				Double D = sc.nextDouble(); //distancia percorrida no treino
+				if (D/T > R) {
+					System.out.println(i);;
+					R = D/T;
+				}
+			}
+		}
+		sc.close();
+	}
+}
