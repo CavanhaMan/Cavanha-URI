6b9ba99dc74956157df076240b94908067b22406
CavanhaMan <cavanhaman@hotmail.com>
2021-08-15 11:56:51 -0300

    Add URI 2787 - ok

---
+++ URI-2787_Xadrez.java
@@ -0,0 +1,21 @@
+/*
++-------------------+
+|Rodrigo CavanhaMan |
+|URI 2787           |
+|Xadrez             |
++-------------------+
+*/
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int L = sc.nextInt();
+		int C = sc.nextInt();
+		if ((L+C)%2==0)
+			System.out.println("1");
+		else
+			System.out.println("0");
+		sc.close();
+	}
+}
